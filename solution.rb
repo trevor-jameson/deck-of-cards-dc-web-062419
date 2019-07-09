c547d164ba382f0b7fbba1912075571248656fbf
Trevor Jameson <tjameson15@gmail.com>
2019-07-09 10:29:49 -0400

    Deal me in!

--- solution.rb
+++ solution.rb
@@ -1,7 +1,29 @@
 class Deck
+    def initialize
+        @cards = []
+        ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"].each do |rank|
+            ["Hearts", "Clubs", "Diamonds", "Spades"].each do |suit|
+                card = Card.new(suit, rank)
+                @cards << card
+            end
+        end
+    end
 
+    def cards
+        @cards
+    end
+
+    def choose_card
+        rand = Random.new(@cards.size).rand
+        @cards.slice!(rand)
+    end
 end
 
 class Card
+    attr_accessor :rank, :suit
 
+    def initialize(suit, rank)
+        @rank = rank
+        @suit = suit
+    end
 end
